59f00d855117235d53ae0e0a5a3c9e813cec24f0
maxxas <maxschamber@gmail.com>
2013-12-10 19:14:44 +0100

    Angabe der Deadline präzisiert

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,8 +4,12 @@ validates :name, presence: true
 validates :deadline, presence: true
 validates :duration, presence: true, numericality: true
 
-def is_delayed?
- self.deadline < Date.today
-end
+	def distance_from_now_in_days
+	  (self.deadline - Date.today).to_i     
+	end
+
+	def is_delayed?
+	 self.deadline < Date.today
+	end
 
 end
