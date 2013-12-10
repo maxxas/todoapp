6c311d64eb7c1c34060bde416dd43ccd6394dfe1
maxxas <maxschamber@gmail.com>
2013-12-10 18:15:22 +0100

    test: deadlines etc. hinzugefügt

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,4 +4,8 @@ validates :name, presence: true
 validates :deadline, presence: true
 validates :duration, presence: true, numericality: true
 
+def is_delayed?
+ self.deadline < Date.today
+end
+
 end
